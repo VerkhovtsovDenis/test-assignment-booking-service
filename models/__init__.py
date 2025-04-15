1015548c96ab4c38e95034e6ca4a800672c163ba
VerkhovtsovDenis <verhovtsov.denis@mail.ru>
2025-04-16 01:05:31 +1000

    refactor

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,13 @@
 from .Base import Base
 from .Table import Table
 from .Reservation import Reservation
+from sqlalchemy import create_engine
+from config import get_db_url
+
+__all__ = (
+    "Base"
+    "Table"
+    "Reservation"
+)
+
+db = create_engine(get_db_url())
