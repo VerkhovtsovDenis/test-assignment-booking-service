07dc0cf12f504ee82d3b714a8aac2fba525ca06b
VerkhovtsovDenis <verkhovtcov.do@students.dvfu.ru>
2025-04-15 20:28:07 +1000

    fix table_id relationship

--- models/Reservation.py
+++ models/Reservation.py
@@ -1,6 +1,7 @@
 from sqlalchemy import String
 from sqlalchemy import DateTime
 from sqlalchemy.orm import Mapped
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import mapped_column
 from sqlalchemy.orm import relationship
 from .Base import Base
@@ -10,11 +11,14 @@ from .Table import Table
 class Reservation(Base):
     __tablename__ = "reservation"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True)   
     customer_name: Mapped[str] = mapped_column(String(60))
-    table_id: Mapped["Table"] = relationship(back_populates="id")
+    table_id: Mapped[int] = mapped_column(ForeignKey("table.id"))
     reservation_time: Mapped[DateTime] = mapped_column(DateTime)
     duration_minutes: Mapped[int] = mapped_column()
+
+    table: Mapped["Table"] = relationship('Table', back_populates="reservations")
+
 
     def __repr__(self) -> str:
         return f"Reservation(id={self.id!r}, "\
