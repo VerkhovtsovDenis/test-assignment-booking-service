44a4e99e75bc5db1d3d95513f1eac946942d411b
VerkhovtsovDenis <verhovtsov.denis@mail.ru>
2025-04-16 11:25:58 +1000

    add standart router from quiq start

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from config import get_db_url
 import uvicorn
+from typing import Union
 
 from routers import routers
 
@@ -12,6 +13,17 @@ app = FastAPI(
 )
 
 [app.include_router(router) for router in routers]
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
 
 if __name__ == "__main__":
     uvicorn.run(app)
