51bc03a35ef7246b42e8916bcc98e96ecf57a23a
VerkhovtsovDenis <verhovtsov.denis@mail.ru>
2025-04-16 11:24:54 +1000

    add router file for table object

--- routers/table.py
+++ routers/table.py
@@ -1,4 +1,32 @@
 from sqlalchemy.ext.asyncio import AsyncSession
-from fastapi import APIRouter
-from fastapi import Depends
+from fastapi import APIRouter, Depends
+from typing import List
+from typing import Annotated
+from schemas import TableSchema
+from models import Table
+from models import db
+from database import get_async_session  # Нужно создать эту функцию
+from sqlalchemy import select
 
+# database = ... 
+
+router = APIRouter(
+    prefix="/tables",
+    tags=["Столы"],
+)
+
+
+@router.get('/', response_model=List[TableSchema])
+async def get_tables(session: AsyncSession = Depends(get_async_session)):
+    print("Получаем таблицы...")  # Для отладки
+    result = await session.execute(select(Table))
+    tables = result.scalars().all()
+    print(f"Найдено {len(tables)} таблиц")  # Для отладки
+    return tables
+
+
+# @router.post('/tables')
+# async def create_table(
+#     session: AsyncSession,
+# ) -> TableSchema:
+#     return await db.session.q
